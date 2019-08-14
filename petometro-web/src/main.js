3c0d77fae34ddd912c06fe76ca96dbe051aed059
tiago.franca <tiago.franca@solutis.com.br>
2019-08-14 14:22:58 -0300

    Adicionado template

--- petometro-web/src/main.js
+++ petometro-web/src/main.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+
+// Components
+import './components'
+
+// Plugins
+import './plugins'
+
+// Sync router with store
+import { sync } from 'vuex-router-sync'
+
+// Application imports
+import App from './App'
+import i18n from '@/i18n'
+import router from '@/router'
+import store from '@/store'
+import Axios from 'axios'
+
+// Sync store with router
+sync(store, router)
+
+Axios.defaults.baseURL = 'http://localhost:56986/api/'
+// if (store.getters[StoreAuthConstants.GETTERS.IS_AUTH]) {
+//   Axios.defaults.headers.common[StoreAuthConstants.STATES.TOKEN] = store.getters[StoreAuthConstants.GETTERS.TOKEN]
+// }
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
+  i18n,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- petometro-web/src/plugins/vuetify.js
+++ petometro-web/src/plugins/vuetify.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import theme from './theme'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.use(Vuetify);
-
-export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
-  },
-});
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+  theme
+})
