b6a172a7bf4f732d24ae40d94476bd47fcace92b
tiago.franca <tiago.franca@solutis.com.br>
2019-08-16 12:42:38 -0300

    Excluindo o projeto

--- petometro-web/src/middleware/auth.js
+++
@@ -1,4 +0,0 @@
-import AccessControlService from '@/api-services/access-control'
-export default function auth ({next}) {
-  AccessControlService.CheckIsAuth(next)
-}
--- petometro-web/src/middleware/guest.js
+++
@@ -1,4 +0,0 @@
-import AccessControlService from '@/api-services/access-control'
-export default function guest ({from, next, router}) {
-  AccessControlService.CheckIsGuest(from, next, router)
-}
